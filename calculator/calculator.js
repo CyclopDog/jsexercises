6c834278d8ce5f82eaf8c85e971b1ac1875c5245
Marcos Fernandes <contato@marcos-fernandes.com>
2019-02-05 18:20:48 -0200

    first commit

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,27 +1,76 @@
-function add () {
-	
+const calcResult = document.getElementById('calcResult');
+const opButton = document.querySelectorAll('.opButton');
+const numButton = document.querySelectorAll('.numButton');
+const acButton = document.getElementById('ac');
+
+let result = 0;
+let numberToCalc = '';
+let calcOp = '';
+
+calcResult.textContent = result;
+
+acButton.addEventListener('click', clearAll);
+
+numButton.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		numberToCalc += button.id;
+		calcResult.textContent = numberToCalc;
+	});
+});
+
+opButton.forEach((button) => {
+	button.addEventListener('click', (e) => {
+		solveCalc();
+		calcOp = button.id;
+	})
+});
+
+function clearAll() {
+	result = 0;
+	numberToCalc = '';
+	calcOp = '';
+	calcResult.textContent = 0;
 }
 
-function subtract () {
+function solveCalc() {
+	switch (calcOp) {
+		case 'plus':
+			result += Number(numberToCalc);
+			numberToCalc = '';
+			calcResult.textContent = result;
+			break;
+
+			case 'minus':
+			result -= Number(numberToCalc);
+			numberToCalc = '';
+			calcResult.textContent = result;
+			break;
+
+			case 'multiply':
+			result *= Number(numberToCalc);
+			numberToCalc = '';
+			calcResult.textContent = result;
+			break;
+
+			case 'power':
+			result = Math.pow(result, numberToCalc);
+			numberToCalc = '';
+			calcResult.textContent = result;
+			break;
+
+			case 'equals':
+			calcOp = '';
+			break;
 	
+		default:
+			result = Number(numberToCalc);
+			numberToCalc = '';
+			calcResult.textContent = result;
+			break;
+	}
 }
 
-function sum () {
-	
-}
-
-function multiply () {
-	
-}
-
-function power() {
-	
-}
-
-function factorial() {
-	
-}
-
+/*
 module.exports = {
 	add,
 	subtract,
@@ -30,3 +79,13 @@ module.exports = {
     power,
 	factorial
 }
+
+function sum () {
+	
+}
+
+function factorial() {
+	
+}
+
+*/
